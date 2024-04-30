27bfb1cd98aeda87451d2ca607f537bd2967a978
krsnktska <yuliya05krasnokutskaja@gmail.com>
2024-04-30 14:11:53 +0300

    initial commit

---
+++ FIgures/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Figures;
+
+internal class Figure
+{
+    public double PositionX;
+    public double PositionY;
+    
+}
+
+
+
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
